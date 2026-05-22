e9e4971d5255a3c77412a6cd5b88fa7befe7a7bf
madmoune <madmoune@hotmail.ca>
2026-05-22 10:17:24 -0400

    WIP

--- src/app/logic/magic-square/magic-square-component/magic-square-component.ts
+++ src/app/logic/magic-square/magic-square-component/magic-square-component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './magic-square-component.html',
   styleUrl: './magic-square-component.scss',
 })
-export class MagicSquareComponent {}
+export class MagicSquareComponent {
+  width = 3;
+
+  square: number[][] = [[]];
+
+  constructor() {
+    this.initSquare();
+  }
+
+  initSquare() {
+    this.square = Array.from({ length: this.width }, () => Array(this.width).fill(0));
+    console.log(this.square)
+  }
+
+  setValue(value: number, x: number, y: number) {
+    this.square[x][y] = value;
+  }
+
+}
