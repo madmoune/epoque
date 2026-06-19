e9870146538337e114ff527a5874ea4776b10773
madmoune <madmoune@hotmail.ca>
2026-06-19 16:03:34 -0400

    Add backlink to popup.

--- src/app/puzzles/shared/puzzle-success-popup/puzzle-success-popup.component.ts
+++ src/app/puzzles/shared/puzzle-success-popup/puzzle-success-popup.component.ts
@@ -1,13 +1,18 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, inject, Input, Output, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 export type PuzzlePopupTone = 'success' | 'partial';
 
 @Component({
   selector: 'app-puzzle-success-popup',
+  imports: [RouterLink],
   templateUrl: './puzzle-success-popup.component.html',
   styleUrl: './puzzle-success-popup.component.scss',
 })
 export class PuzzleSuccessPopupComponent {
+  private readonly router = inject(Router);
+  protected readonly dismissed = signal(false);
+
   @Input({ required: true }) title = '';
   @Input() message = '';
   @Input() answer = '';
@@ -16,6 +21,20 @@ export class PuzzleSuccessPopupComponent {
   @Input() tone: PuzzlePopupTone = 'success';
 
   @Output() readonly action = new EventEmitter<void>();
+
+  protected get menuFragment(): string {
+    return this.router.url.split(/[?#]/)[0].replace(/^\/+/, '');
+  }
+
+  protected dismissFromBackground(event: Event): void {
+    const target = event.target;
+
+    if (target instanceof Element && target.closest('button, a')) {
+      return;
+    }
+
+    this.dismissed.set(true);
+  }
 
   @HostListener('document:keydown.enter', ['$event'])
   protected handleEnter(event: Event): void {
